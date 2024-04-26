4da869b3cf773f5691178eff412cc4278c3b39fe
koustubh1012 <koustubh1012@gmail.com>
2024-04-26 11:47:38 -0400

    created scubscription for camera feed

--- src/horizon/horizon/detect_horizon.py
+++ src/horizon/horizon/detect_horizon.py
@@ -1,6 +1,47 @@
-def main():
+import rclpy
+from rclpy.node import Node
+from rclpy.qos import qos_profile_sensor_data
+from cv_bridge import CvBridge
+import cv2
+from sensor_msgs.msg import Image
+
+
+class HorizonDetector(Node):
+
+    def __init__(self):
+        super().__init__('minimal_subscriber')
+        self.subscription = self.create_subscription(Image,'/camera/image_raw',self.camera_callback,qos_profile_sensor_data)
+        self.subscription  # prevent unused variable warning
+
+    def camera_callback(self, msg):
+        bridge = CvBridge()
+        image = bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
     print('Hi from horizon.')
+
+    node = HorizonDetector()
+
+    rclpy.spin(node)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    node.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
     main()
+
+# def main():
+#     print('Hi from horizon.')
+
+
+# if __name__ == '__main__':
+#     main()
